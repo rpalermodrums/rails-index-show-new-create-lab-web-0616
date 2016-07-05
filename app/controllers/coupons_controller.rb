bb283a8f95f9136f10f4441f2013c8d528d20d52
Ryan Palermo <stix250@gmail.com>
2016-07-05 11:02:25 -0400

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,68 @@
+class CouponsController < ApplicationController
+  before_action :set_coupon, only: [:show, :edit, :update, :destroy]
+
+  # GET /coupons
+  # GET /coupons.json
+  def index
+    @coupons = Coupon.all
+  end
+
+  # GET /coupons/1
+  # GET /coupons/1.json
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  # GET /coupons/new
+  def new
+  end
+
+  # GET /coupons/1/edit
+  def edit
+  end
+
+  # POST /coupons
+  # POST /coupons.json
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
+  end
+
+  # PATCH/PUT /coupons/1
+  # PATCH/PUT /coupons/1.json
+  def update
+    respond_to do |format|
+      if @coupon.update(coupon_params)
+        format.html { redirect_to @coupon, notice: 'Coupon was successfully updated.' }
+        format.json { render :show, status: :ok, location: @coupon }
+      else
+        format.html { render :edit }
+        format.json { render json: @coupon.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /coupons/1
+  # DELETE /coupons/1.json
+  def destroy
+    @coupon.destroy
+    respond_to do |format|
+      format.html { redirect_to coupons_url, notice: 'Coupon was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_coupon
+      @coupon = Coupon.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def coupon_params
+      params.require(:coupon).permit(:coupon_code, :store)
+    end
+end
